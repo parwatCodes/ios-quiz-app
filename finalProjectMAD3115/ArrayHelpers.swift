68c1213ddbb76c6c1dfc1876c077ee476ad4cc81
Parwat Kunwar <parwatkunwar08@gmail.com>
2022-06-23 13:46:17 -0400

    Add UI to select the options and highlight selected opt

--- finalProjectMAD3115/ArrayHelpers.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ArrayHelpers.swift
-//  finalProjectMAD3115
-//
-//  Created by Parwat Kunwar on 2022-06-22.
-//
-
-import Foundation
-
---
+++ finalProjectMAD3115/functions.swift
@@ -0,0 +1,8 @@
+//
+//  functions.swift
+//  finalProjectMAD3115
+//
+//  Created by Parwat Kunwar on 2022-06-23.
+//
+
+import Foundation
